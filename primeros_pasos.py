7bdd62ac493ba302875ce26327b1347ec68a6a32
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2024-05-16 00:38:09 -0500

    Final de la primera parte

---
+++ primeros_pasos.py
@@ -0,0 +1,97 @@
+# print("soy camilo")
+# print("Estoy haciendo un curso de python")
+
+# print(5+10) #Esta es la forma de hacer comentarios en python '''
+
+# # ''' Triple comilla es otra forma de comentar '''
+
+# # Clase - Variables
+# my_name = "Camilo"
+# print(my_name)
+
+# My_age = 28
+# print(My_age)
+
+# # input
+# my_name = input("Dime tu numbre: ")
+# print("usando input", my_name)
+
+# # strings
+# Name = "camilo"
+
+#Clase 4 - Strings
+name = "Camilo"
+last_name= "Losada"
+age = 28
+
+impresion = f"Hola {name} {last_name}, tu edad es {age}"
+print(impresion)
+
+# Clase 5 - Numbers
+lives = 3
+print(type(lives))
+age = 12
+budget = 100
+
+temperature = 12.12
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+number = 4500000000000000000.1
+print(number)
+
+number_b = 0.0000000000000001
+print(number_b)
+
+# clase 6 - Booleans
+is_single = True
+print(type(is_single))
+is_single = False
+print(is_single)
+
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
+
+# Clase 7 - Transform types
+name = "Nicolas"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("Nicolas" + " Molina")
+print(10 + 20)
+print("Nicolas" + "12")
+
+age = 12
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = input('Escribe tu edad => ')
+print(type(age))
+age = int(age)
+age += 10
+print(f'Tu edad en 10 aÃ±os serÃ¡ {age}')
